6bcd40efa741debc894f88c8e8eb2f17fb96550a
unitiincristo <unitiincristopadova@gmail.com>
2026-09-21 14:38:09 +0200

    Update version to 1.5.0

--- app.py
+++ app.py
@@ -122,4 +122,4 @@ if uploaded_file is not None:
                 st.error(f"Si è verificato un errore: {e}")
 
 st.markdown("---")
-st.caption("Versione 1.4.6 - Motore Solo Testo con iniezione dinamica fontbuffer e fallback sicuro")
+st.caption("Versione 1.5.0 - Motore di gestione intelligente della spaziatura orizzontale (Zero Sovrapposizioni)")
